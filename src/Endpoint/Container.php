07b95d452dde6b564738c443f658e98965f76c77
Ostap Mykhaylyak <me@ostap.dev>
2025-08-17 21:34:11 +0200

    Create Container.php

---
+++ src/Endpoint/Container.php
@@ -0,0 +1,30 @@
+<?php
+namespace Ostap\Nube\Endpoint;
+
+use Ostap\Nube\Client;
+
+class Container
+{
+    protected Client $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public function list(): array
+    {
+        return $this->client->request('GET', '/1.0/containers');
+    }
+
+    public function create(string $name, array $config = []): array
+    {
+        $data = array_merge(['name' => $name], $config);
+        return $this->client->request('POST', '/1.0/containers', $data);
+    }
+
+    public function delete(string $name): array
+    {
+        return $this->client->request('DELETE', "/1.0/containers/{$name}");
+    }
+}
