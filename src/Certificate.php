4f3d1a779aba5555506ccac906f4c5a2cab7f205
Ostap Mykhaylyak <me@ostap.dev>
2025-08-17 22:59:55 +0200

    Create Certificate.php

---
+++ src/Certificate.php
@@ -0,0 +1,36 @@
+<?php
+namespace Ostap\Nube;
+
+class Certificate
+{
+    public static function generate(string $certPath, string $keyPath, string $cn = 'OstapNube'): array
+    {
+        $dn = [
+            "countryName" => "IT",
+            "stateOrProvinceName" => "Italy",
+            "localityName" => "City",
+            "organizationName" => "OstapNube",
+            "commonName" => $cn,
+        ];
+
+        $privKey = openssl_pkey_new([
+            'private_key_bits' => 2048,
+            'private_key_type' => OPENSSL_KEYTYPE_RSA
+        ]);
+
+        $csr = openssl_csr_new($dn, $privKey);
+        $cert = openssl_csr_sign($csr, null, $privKey, 365);
+
+        openssl_x509_export_to_file($cert, $certPath);
+        openssl_pkey_export_to_file($privKey, $keyPath);
+
+        // restituisce cert e chiave in formato stringa per invio a LXD
+        openssl_x509_export($cert, $certOut);
+        openssl_pkey_export($privKey, $keyOut);
+
+        return [
+            'cert' => $certOut,
+            'key'  => $keyOut
+        ];
+    }
+}
