211702513933ff28ff8e4d079d3f56006fd38758
Ostap Mykhaylyak <me@ostap.dev>
2025-10-06 06:08:27 +0200

    Create Operation.php

---
+++ src/Endpoint/Operation.php
@@ -0,0 +1,54 @@
+<?php
+namespace Ostap\Nube\Endpoint;
+
+use Ostap\Nube\Client;
+
+class Operation
+{
+    protected Client $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public function list(): array
+    {
+        return $this->client->request('GET', '/1.0/operations');
+    }
+
+    public function get(string $id): array
+    {
+        $id = ltrim($id, '/');
+        if (!str_starts_with($id, '1.0/operations/')) {
+            $id = '1.0/operations/' . $id;
+        }
+        
+        return $this->client->request('GET', "/{$id}");
+    }
+
+    public function cancel(string $id): array
+    {
+        $id = ltrim($id, '/');
+        if (!str_starts_with($id, '1.0/operations/')) {
+            $id = '1.0/operations/' . $id;
+        }
+        
+        return $this->client->request('DELETE', "/{$id}");
+    }
+
+    public function wait(string $id, int $timeout = 300): array
+    {
+        $id = ltrim($id, '/');
+        if (!str_starts_with($id, '1.0/operations/')) {
+            $id = '1.0/operations/' . $id;
+        }
+        
+        return $this->client->request('GET', "/{$id}/wait?timeout={$timeout}");
+    }
+
+    public function getLog(string $id, string $logPath): array
+    {
+        return $this->client->request('GET', $logPath);
+    }
+}
